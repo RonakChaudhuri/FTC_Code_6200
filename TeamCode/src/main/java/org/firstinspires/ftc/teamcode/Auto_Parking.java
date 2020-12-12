ce4556d8cc3511594755cc65094e2d00092b11cf
ronakchaudhuri <sportsronak@gmail.com>
2020-12-11 19:40:36 -0600

    First Commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_Parking.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_Parking.java
@@ -47,7 +47,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  * It includes all the skeletal structure that all linear OpModes contain.
  *
  * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list.
  */
 
 @Autonomous(name="Auto_Parking", group="Linear Opmode")
